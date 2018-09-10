42718019990c2f39148c8016c8b34662fc374bd9
Rafael Baena Neto <rafael.baena@gmail.com>
2018-09-10 07:13:05 -0300

    ADICIONADO MODULO ATRAÇÕES

--- core/models.py
+++ core/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from atracoes.models import Atracao
 
 
 class PontoTuristico(models.Model):
     nome = models.CharField('Nome', max_length=150)
     descricao = models.TextField('Descrição')
     aprovado = models.BooleanField('Aprovado?', default=False)
+    atracoes = models.ManyToManyField(
+        Atracao, verbose_name='Atrações', related_name='atraco'
+    )
     criado_em = models.DateTimeField('Criado em', auto_now_add=True)
     atualizado_em = models.DateTimeField('Atualizado em', auto_now_add=True)
 
