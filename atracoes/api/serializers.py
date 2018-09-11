d1a1f98a59484bdb043e6941b880ddff1070cb51
Rafael Baena Neto <rafael.baena@gmail.com>
2018-09-11 16:15:48 -0300

    ADICIONADO MÓDULO VIA API DE ATRAÇÕES

---
+++ atracoes/api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import ModelSerializer
+from atracoes.models import Atracao
+
+
+class AtracaoSerializer(ModelSerializer):
+    class Meta:
+        model = Atracao
+        fields = ('id', 'descricao', 'horario_func', 'idede_minima')
--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
+from atracoes.api.viewsets import AtracaoViewSet
 
 """ Particularidade do Django Rest Framework """
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
+router.register(r'atracao', AtracaoViewSet)
 
 urlpatterns = [
     path('',include(router.urls)),
