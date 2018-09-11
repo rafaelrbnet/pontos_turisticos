bdb53e654db27f51b1a29b1e70d2aa2687b83b59
Rafael Baena Neto <rafael.baena@gmail.com>
2018-09-11 09:55:48 -0300

    ADICIONADO MODULO API DE PONTOS TURÍSTICOS

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from core.api.viewsets import PontoTuristicoViewSet
+
+""" Particularidade do Django Rest Framework """
+router = routers.DefaultRouter()
+router.register(r'pontoturistico', PontoTuristicoViewSet)
 
 urlpatterns = [
+    path('',include(router.urls)),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
 ]
