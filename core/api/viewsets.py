2f70c3a16886339dbe2714ad2e07f29d0a56eec2
Rafael Baena Neto <rafael.baena@gmail.com>
2018-09-12 07:34:24 -0300

    MODIFICADO list NA API DE PONTOS TURÍSTICOS PARA MODIFICAR O RETORNO PADRÃO DO REST FRAME WORK ADICIONADO create NA API DE PONTOS TURÍSTICOS PARA MODIFICAR O RETORNO PADRÃO DO REST FRAME WORK NA INSERÇÃO DE NOVOS REGISTROS

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from django.contrib.auth.models import User
@@ -12,7 +13,23 @@ class PontoTuristicoViewSet(ModelViewSet):
         return PontoTuristico.objects.filter(aprovado=True)
 
     def list(self, request, *args, **kwargs):
-        return Response({'teste': 123})
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({'Listagem de pontos turísticos': serializer.data})
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        return Response({'Criado com sucesso': serializer.data}, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class CurrentUserViewSet(ReadOnlyModelViewSet):
