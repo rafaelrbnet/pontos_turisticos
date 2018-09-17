6d33acfc253bf976ec831cbea6e32564d0b8f78e
Rafael Baena Neto <rafael.baena@gmail.com>
2018-09-17 10:11:42 -0300

    Adicionada SearchFilter nos demis métodos de todas as APIS

--- atracoes/api/viewsets.py
+++ atracoes/api/viewsets.py
@@ -1,3 +1,4 @@
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 from atracoes.models import Atracao
 from .serializers import AtracaoSerializer
@@ -6,4 +7,5 @@ from .serializers import AtracaoSerializer
 class AtracaoViewSet(ModelViewSet):
     queryset = Atracao.objects.all()
     serializer_class = AtracaoSerializer
-    filter_fields = ('id', 'descricao', 'idede_minima')
+    filter_backends = (SearchFilter,)
+    search_fields = ('id', 'descricao', 'idede_minima')
--- enderecos/api/viewsets.py
+++ enderecos/api/viewsets.py
@@ -1,4 +1,6 @@
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
+
 from enderecos.models import Endereco
 from .serializers import EnderecoSerializer
 
@@ -6,3 +8,5 @@ from .serializers import EnderecoSerializer
 class EnderecoViewSet(ModelViewSet):
     queryset = Endereco.objects.all()
     serializer_class = EnderecoSerializer
+    filter_backends = (SearchFilter,)
+    search_fields = ('localizacao', 'referencia', 'cidade', 'estado', 'pais')
