dee4ea13494f0b362c3744b113f3c5b69436fd26
Rafael Baena Neto <rafael.baena@gmail.com>
2018-09-18 12:53:39 -0300

    Adicionada relacionamento nos objetos serializados (nested-relationships) http://www.django-rest-framework.org/api-guide/relations/#nested-relationships

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,12 +1,24 @@
 from rest_framework.serializers import ModelSerializer
 from django.contrib.auth.models import User
 from core.models import PontoTuristico
+from atracoes.api.serializers import AtracaoSerializer
+from comentarios.api.serializers import ComentarioSerializer
+from avaliacoes.api.serializers import AvaliacaoSerializer
+from enderecos.api.serializers import EnderecoSerializer
 
 
 class PontoTuristicoSerializer(ModelSerializer):
+    atracoes = AtracaoSerializer(many=True)
+    comentarios = ComentarioSerializer(many=True)
+    avaliacoes = AvaliacaoSerializer(many=True)
+    enderecos = EnderecoSerializer()
+
     class Meta:
         model = PontoTuristico
-        fields = ('id', 'nome', 'descricao', 'aprovado', 'foto')
+        fields = (
+            'id', 'nome', 'descricao', 'aprovado', 'foto',
+            'atracoes', 'comentarios', 'avaliacoes', 'enderecos'
+        )
 
 
 class CurrentUserSerializer(ModelSerializer):
