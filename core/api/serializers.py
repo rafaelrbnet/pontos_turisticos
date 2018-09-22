0caea9d1dc4f63a194daf2990bd5d55dabf13de6
Rafael Baena Neto <rafael.baena@gmail.com>
2018-09-22 09:56:09 -0300

    Implementada tecnica de Nested Relationships para lidar com os relaciomentos da API core

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework.fields import SerializerMethodField, DateTimeField
 from rest_framework.serializers import ModelSerializer
 from django.contrib.auth.models import User
+
+from atracoes.models import Atracao
+from comentarios.models import Comentario
+from avaliacoes.models import Avaliacao
+from enderecos.models import Endereco
 from core.models import PontoTuristico
 from atracoes.api.serializers import AtracaoSerializer
 from comentarios.api.serializers import ComentarioSerializer
@@ -13,7 +18,7 @@ class PontoTuristicoSerializer(ModelSerializer):
     comentarios = ComentarioSerializer(many=True)
     avaliacoes = AvaliacaoSerializer(many=True)
     enderecos = EnderecoSerializer()
-    descricao_completa = SerializerMethodField()
+    descricao_completa = SerializerMethodField(read_only=True)
     criado_em = DateTimeField(read_only=True, format="%d/%m/%Y %H:%M:%S")
 
     class Meta:
@@ -24,8 +29,45 @@ class PontoTuristicoSerializer(ModelSerializer):
             'descricao_completa', 'criado_em'
         )
 
+    def cria_atracoes(self, atracoes, ponto):
+        for atracao in atracoes:
+            at = Atracao.objects.create(**atracao)
+            ponto.atracoes.add(at)
+
+    def cria_comentarios(self, comentarios, ponto):
+        for comentario in comentarios:
+            co = Comentario.objects.create(**comentario)
+            ponto.comentarios.add(co)
+
+    def cria_avaliacoes(self, avaliacoes, ponto):
+        for avaliacao in avaliacoes:
+            av = Avaliacao.objects.create(**avaliacao)
+            ponto.avaliacoes.add(av)
+
+    def create(self, validated_data):
+        atracoes = validated_data['atracoes']
+        enderecos = validated_data['enderecos']
+        comentarios = validated_data['comentarios']
+        avaliacoes = validated_data['avaliacoes']
+        del validated_data['atracoes']
+        del validated_data['enderecos']
+        del validated_data['comentarios']
+        del validated_data['avaliacoes']
+
+        ponto = PontoTuristico.objects.create(**validated_data)
+
+        self.cria_atracoes(atracoes, ponto)
+        self.cria_comentarios(comentarios, ponto)
+        self.cria_avaliacoes(avaliacoes, ponto)
+
+        end = Endereco.objects.create(**enderecos)
+        ponto.enderecos = end
+
+        ponto.save()
+        return ponto
+
     def get_descricao_completa(self, obj):
-        return '{} - {}'.format(obj.nome, obj.descricao)
+        return '%s - %s' % (obj.nome, obj.descricao)
 
 
 class CurrentUserSerializer(ModelSerializer):
